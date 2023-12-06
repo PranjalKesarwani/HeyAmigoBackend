7f41bb4290c8a77beb128fd659e2aa853d578815
Pranjal Kesarwani <89082511+PranjalKesarwani@users.noreply.github.com>
2023-12-07 00:21:43 +0530

    tired

--- src/app.ts
+++ src/app.ts
@@ -25,12 +25,9 @@ app.use(cors({
     
 }));
 app.use((req: any, res: Response, next) => {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-     
-    }
+    res.header("Access-Control-Allow-Origin", origin);
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+   
     next();
 });
 
