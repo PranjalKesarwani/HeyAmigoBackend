ac5e10b14bf400941c1dc4d3e41f8c8a2f23e15b
Pranjal Kesarwani <89082511+PranjalKesarwani@users.noreply.github.com>
2023-12-08 14:52:14 +0530

    trying

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("./config/server");
-const PORT =  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 const messageRoutes_1 = __importDefault(require("./routes/messageRoutes"));
@@ -29,16 +29,20 @@ const origin = "https://heyamigo.netlify.app/";
 // }));
 app.use((0, cors_1.default)({
     origin: origin,
-    credentials: true
-}));
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.header(
-        "Access-Control-Allow-Headers"
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ['Content-Type'],
 
-    );
-    next();
-});
+    optionsSuccessStatus: 204
+}));
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//     res.header(
+//         "Access-Control-Allow-Headers"
+
+//     );
+//     next();
+// });
 // app.use((req: any, res: Response, next) => {
 //     // res.header("Access-Control-Allow-Origin", origin);
 //     // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
